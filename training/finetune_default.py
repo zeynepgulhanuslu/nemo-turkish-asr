0e8190da5450f5185bcc4e79592ec7e1b60d9f49
ZEYNEP GULHAN USLU <zeynep.uslu@turkcell.com.tr>
2022-10-21 14:14:58 +0300

    some changes

--- training/finetune_default.py
+++ training/finetune_default.py
@@ -44,7 +44,7 @@ def write_processed_manifest(data, original_path):
 
     manifest_dir = os.path.split(original_path)[0]
     filepath = os.path.join(manifest_dir, new_manifest_name)
-    with open(filepath, 'w') as f:
+    with open(filepath, 'w', encoding='utf-8') as f:
         for datum in tqdm(data, desc="Writing manifest data"):
             datum = json.dumps(datum)
             f.write(f"{datum}\n")
