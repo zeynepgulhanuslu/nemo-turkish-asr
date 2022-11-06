058ef8e1db303909a353bb9486f035d661f4574e
zeynepgulhanuslu <zeynepgulhanuslu@gmail.com>
2022-11-06 18:44:25 +0300

    bug fix

--- training/train_conformer_ctc.py
+++ training/train_conformer_ctc.py
@@ -36,12 +36,12 @@ def main(cfg):
     dev_charset = get_charset(dev_manifest_data)
     train_dev_set = set.union(set(train_charset.keys()), set(dev_charset.keys()))
 
-    print('labels:', cfg.labels)
     asr_model = EncDecCTCModel(cfg=cfg.model, trainer=trainer)
+    '''
     asr_model.change_vocabulary(new_vocabulary=list(train_dev_set))
     asr_model.setup_training_data(cfg.train_ds)
     asr_model.setup_multiple_validation_data(cfg.validation_ds)
-
+    '''
     # Initialize the weights of the model from another model, if provided via config
     asr_model.maybe_init_from_pretrained_checkpoint(cfg)
 
