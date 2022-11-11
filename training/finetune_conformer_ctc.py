66f05b2fd678bf1ae44b0b43291cc7f0689c6854
ZEYNEP GULHAN USLU <zeynep.uslu@turkcell.com.tr>
2022-11-11 17:18:37 +0300

    some changes

--- training/finetune_conformer_ctc.py
+++ training/finetune_conformer_ctc.py
@@ -20,22 +20,14 @@ def main(cfg):
 
     asr_model = EncDecCTCModelBPE.from_pretrained(model_name="stt_en_conformer_ctc_small")
     asr_model.change_vocabulary(new_tokenizer_dir=cfg.model.tokenizer.dir, new_tokenizer_type=cfg.model.tokenizer.type)
-    '''
-    default_cfg = copy.deepcopy(asr_model.cfg)
-    new_preprocessor_config = copy.deepcopy(cfg.model.preprocessor)
-    new_preprocessor = asr_model.from_config_dict(new_preprocessor_config)
-    asr_model.preprocessor = new_preprocessor
-    cfg.model.preprocessor = new_preprocessor_config
-    asr_model.cfg = default_cfg
+
     asr_model.setup_training_data(train_data_config=cfg.model.train_ds)
     asr_model.setup_validation_data(val_data_config=cfg.model.validation_ds)
     asr_model.setup_optimization(optim_config=cfg.model.optim)
-    new_spec_augment_config = copy.deepcopy(cfg.model.spec_augment)
-    asr_model.cfg.spec_augment = new_spec_augment_config
-    asr_model.spec_augment = asr_model.from_config_dict(asr_model.cfg.spec_augment)
-    '''
+
+
     # Initialize the weights of the model from another model, if provided via config
-    asr_model.maybe_init_from_pretrained_checkpoint(cfg)
+    #asr_model.maybe_init_from_pretrained_checkpoint(cfg)
 
     trainer.fit(asr_model)
     #asr_model.save_to('conformer_smaii_128_hi.nemo')
